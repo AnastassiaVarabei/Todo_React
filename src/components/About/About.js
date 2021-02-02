85c7aee5728f8d58005d0c7ec2271ebfa5a04c5e
AnastassiaVarabei <61653983+AnastassiaVarabei@users.noreply.github.com>
2021-02-02 10:44:20 +0300

    Add files via upload

---
+++ src/components/About/About.js
@@ -0,0 +1,137 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import {Octokit} from '@octokit/rest';
+import RepoList from "../RepoList/RepoList";
+
+import styles from "./About.module.css";
+
+const octokit = new Octokit();
+
+class About extends React.Component {
+    state ={
+        username: 'AnastassiaVarabei',
+        isLoading: true,
+        repoList: [],
+        errorText: 'Возникла ошибка при получении данных',
+        isError: false,
+        userData: [],
+        firstRepo: 0,
+        lastRepo: 5
+    };
+
+    componentDidMount() {
+        octokit.repos.listForUser({
+            username: this.state.username
+        })
+            .then(({ data }) => {
+                this.setState({
+                    repoList: data,
+                    isLoading: false
+                });
+            })
+            .catch(err => {
+                this.setState({
+                    isLoading: false,
+                    isError: true,
+                    errorText: err
+                })
+            });
+
+        octokit.users.getByUsername({
+            username: this.state.username
+        })
+            .then(({data}) => {
+                this.setState({
+                    userData: data,
+                    isLoading: false,
+                })
+            })
+            .catch(err => {
+                this.setState({
+                    isLoading: false,
+                    isError: true,
+                    errorText: err
+                })
+            });
+    };
+
+    onClickNext = () => {
+        this.setState({
+            firstRepo: this.state.firstRepo + 5,
+            lastRepo: this.state.lastRepo + 5
+        });
+    };
+
+    onClickBack = () => {
+        this.setState({
+            firstRepo: this.state.firstRepo - 5,
+            lastRepo: this.state.lastRepo - 5
+        });
+    };
+
+    render() {
+        const {isLoading, isError, errorText, repoList, userData} = this.state;
+
+        if (!isError)
+        return (
+            <div className={styles.wrapper}>
+                <div className={styles.userInfo}>
+                    <h1 className={styles.userInfo__header}>
+                        {userData.name} <a href={userData.html_url} target="__blank">{userData.login}</a>
+                    </h1>
+                    <p>
+                        <img className={styles.avatar} src={userData.avatar_url} alt='Фото профиля' />
+                        <div>
+                            <p className={styles.avatar__title}> {userData.name} </p>
+                            <p className={styles.avatar__subtitle}> {userData.bio} </p>
+                        </div>
+                    </p>
+                </div>
+
+                <Card>
+                    <div className={styles.repos}>
+                        {isLoading ?
+                            <div>
+                                <h3> Список репозиториев на github.com </h3>
+                                <CircularProgress/>
+                            </div> :
+                            <div>
+                                {isError ?
+                                    <div>
+                                        <h3> Что-то пошло не так. Невозможно отобразить список репозиториев :( </h3>
+                                    </div> :
+                                    <div>
+                                        {repoList.length === 0 ?
+                                            <div>
+                                                <h3> Список репозиториев на github.com. К сожалению у Вас нет ниодного репозитория... </h3>
+                                            </div> :
+                                            <RepoList
+                                                repoList={repoList}
+                                                infoOfUser={userData}
+                                                onClickNext={this.onClickNext}
+                                                onClickBack={this.onClickBack}
+                                                firstRepo={this.state.firstRepo}
+                                                lastRepo={this.state.lastRepo}
+                                            />
+                                        }
+                                    </div>
+                                }
+                            </div>
+                        }
+                    </div>
+                </Card>
+
+
+            </div>
+        );
+        else
+            return (
+                <h2 className={styles.errorText}>
+                    {errorText}...
+                </h2>
+            )
+    };
+}
+
+export default About;
---
+++ src/components/App/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import Card from "@material-ui/core/Card";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+
+import Todo from "../Todo/Todo";
+import About from "../About/About";
+import Contacts from "../Contacts/Contacts";
+
+import styles from "./App.module.css";
+
+const App = () => (
+    <Router>
+        <div className={styles.wrap}>
+            <Card className={styles.sidebar}>
+                <MenuList className={styles.menu}>
+                    <Link className={styles.link} to='/'><MenuItem>Обо мне</MenuItem></Link>
+                    <Link className={styles.link} to='/todo'><MenuItem>Дела</MenuItem></Link>
+                    <Link className={styles.link} to='/contacts'><MenuItem>Контакты</MenuItem></Link>
+                </MenuList>
+            </Card>
+
+            <Card className={styles.content}>
+                <Route path='/' exact component={About} />
+                <Route path='/todo' component={Todo} />
+                <Route path='/contacts' component={Contacts} />
+            </Card>
+        </div>
+    </Router>);
+
+export default App;
